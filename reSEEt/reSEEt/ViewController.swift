9da6de2a9867e14213b49bd682077f71099c438f
calebkuo99 <calebkuo@berkeley.edu>
2019-02-16 01:44:16 -0800

    pushing app stuff

---
+++ reSEEt/reSEEt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reSEEt
+//
+//  Created by Caleb Kuo on 2/16/19.
+//  Copyright © 2019 Caleb Kuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
